e1137744a270d782547ebd7cb9c5f9074802f3d7
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2023-01-12 15:57:32 +0100

    Added ticket model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Event, Ticket
+
 # Register your models here.
+admin.register(Event)
+admin.register(Ticket)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Event(models.Model):
     name = models.CharField(max_length=64)
@@ -7,4 +9,11 @@ class Event(models.Model):
     category = models.CharField(max_length=16)
     thumbnail = models.URLField()
     page = models.URLField()
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Ticket(models.Model):
+    price = models.FloatField()
+    event = models.ForeignKey(Event, models.CASCADE)
+    user = models.ForeignKey(User, models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
