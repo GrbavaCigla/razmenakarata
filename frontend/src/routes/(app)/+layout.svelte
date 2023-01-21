9373190512d8b8ddbd23f60a530604d18ba16a0d
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2023-01-21 16:54:55 +0100

    Routes refactored, Started working on login route

---
+++ frontend/src/routes/(app)/+layout.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Navbar from "../../components/Navbar.svelte";
+  import Sidebar from "../../components/Sidebar.svelte";
+</script>
+
+<div class="drawer">
+  <input id="main-drawer" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content">
+    <div class="container mx-auto flex flex-col gap-4 p-4 h-full">
+      <div class="sticky top-4 z-20 w-full">
+        <Navbar />
+      </div>
+
+      <slot />
+
+      <footer class="shadow mt-auto rounded-box p-4 bg-base-100">
+        <!-- TODO: Add logo -->
+        <p>Copyright © 2023 - All right reserved</p>
+      </footer>
+    </div>
+  </div>
+
+  <div class="drawer-side">
+    <label for="main-drawer" class="drawer-overlay"></label>
+    <ul class="menu gap-2 p-4 w-80 bg-base-100">
+      <Sidebar />
+      <div class="divider my-0"></div>
+      <div class="btn btn-ghost">Ulogujte se</div>
+      <div class="btn btn-primary">Prodajte kartu</div>
+    </ul>
+  </div>
+</div>
--- frontend/src/routes/(app)/+page.svelte
+++ frontend/src/routes/(app)/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import Navbar from "../components/Navbar.svelte";
-  import EventGrid from "../components/EventGrid.svelte";
-  import Sidebar from "../components/Sidebar.svelte";
+  import Navbar from "../../components/Navbar.svelte";
+  import EventGrid from "../../components/EventGrid.svelte";
+  import Sidebar from "../../components/Sidebar.svelte";
   import type { PageData } from "./$types";
 
   export let data: PageData;
