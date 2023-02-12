a732957c8eeaa83a7343887326dfe1f648097a42
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2023-02-12 14:15:02 +0100

    Account menu WIP

--- frontend/src/components/LoginLinkButton.svelte
+++ frontend/src/components/LoginLinkButton.svelte
@@ -1,18 +1,43 @@
+<!-- TODO: Finish this. IMPORTANT -->
 <script lang="ts">
-  import { access_token } from "$stores/auth";
+  import { notifications } from "$src/stores/notification";
+  import { access_token, refresh_token } from "$stores/auth";
+  import { onMount } from "svelte";
+
+  function logout() {
+    $notifications = [...$notifications, "Uspešno ste se izlogovali sa Vašeg naloga."];
+    $access_token = null;
+    $refresh_token = null;
+  }
+
+  onMount(() => {
+    console.log($access_token);
+  })
 </script>
 
-<!-- TODO: This check apparently doesn't work -->
+<!-- TODO: $access_token loads too late -->
 {#if $access_token == null}
   <a class="btn btn-ghost" href="/login">Ulogujte se</a>
 {:else}
-  <a href="/account">
-    <div class="avatar placeholder hidden lg:inline-flex">
+  <div class="dropdown dropdown-end hidden lg:inline-block">
+    <div class="avatar placeholder">
       <div class="bg-neutral text-neutral-content mask mask-squircle h-10 w-10">
         <!-- TODO: Add user initials -->
-        <span class="text-lg font-bold">JD</span>
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label tabindex="0" class="btn no-animation text-lg font-bold m-1"
+          >JD</label
+        >
       </div>
     </div>
-  </a>
-  <a class="btn btn-ghost lg:hidden" href="/account">Nalog</a>
+    <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+    <ul
+      tabindex="0"
+      class="dropdown-content menu p-2 mt-1 shadow bg-base-100 rounded-box w-52"
+    >
+      <li><button on:click="{logout}">Izlogujte se</button></li>
+      <li><a href="/account">Moj nalog</a></li>
+    </ul>
+  </div>
+  <!-- <a class="btn btn-ghost lg:hidden" href="/account">Nalog</a> -->
 {/if}
