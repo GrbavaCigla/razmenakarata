b399627ed90e99a2709e59ec6dff13898efabc93
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2023-02-12 14:16:09 +0100

    Docker WIP

---
+++ docker/backend/server-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd $WORKDIR/backend
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+# python manage.py createsuperuser --noinput
+gunicorn razmenakarata.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
---
+++ docker/backend/worker-entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd $WORKDIR/backend
+
+python manage.py run_huey
