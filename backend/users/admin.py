55bf9b703dfeb962515c48b19234d34fd7591c7f
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2024-07-18 20:54:32 +0200

    Minor fixes to backend model

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Preferences
+from .models import Preferences, User
 
 # Register your models here.
-admin.register(Preferences)
+admin.site.register(Preferences)
+admin.site.register(User)
--- backend/users/models.py
+++ backend/users/models.py
@@ -19,10 +19,19 @@ class CustomUserManager(UserManager):
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
-    email_confirmed = models.BooleanField(default=False)
-    
     if settings.DELETE_USERNAME:
         REQUIRED_FIELDS = ["first_name", "last_name"]
         USERNAME_FIELD = "email"
